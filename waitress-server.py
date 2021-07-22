898c77238b05219010433b00da08580bc0d40e39
qone2 <whrbdnjs33@naver.com>
2021-07-22 10:13:46 +0900

    버그 수정

--- waitress-server.py
+++ waitress-server.py
@@ -1,5 +1,6 @@
 from waitress import serve
 import app
 
-# serve(app, host="0.0.0.0", port=5000)
-serve(app, port=5000)
+if __name__ == "__main__":
+    # serve(app.app, host="0.0.0.0", port=5000)
+    serve(app.app, port=5000)
